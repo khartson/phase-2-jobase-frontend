71b78b040564a5deedf819937be9e6a539ba0477
Kyle <khartson2017@gmail.com>
2022-03-10 15:18:35 -0500

    added form state functionality

--- src/components/NewJobForm.js
+++ src/components/NewJobForm.js
@@ -1,8 +1,35 @@
 import { React, useState } from 'react'; 
-import {Button, Modal, Form, Row, Col} from 'react-bootstrap';
+import {Button, Modal, Form, Row, Col, InputGroup} from 'react-bootstrap';
 
 function NewJobForm({ show, handleClose }) {
+    const [formJob, setFormJob] = useState(
+        {
+            title: '',
+            company: '',
+            location: '',
+            companySite: '',
+            contactName: '',
+            technologies: [],
+            applied: false,
+            replied: false
+        }
+    )
+
+    const [technology, setTechnology] = useState(); 
     
+    function handleFormChange(event) {
+        const category =    event.target.id;
+        const input    = event.target.value;
+        console.log('category: ', category, "input: ", input)
+        setFormJob({
+                    ...formJob, 
+                    [category]: input
+                   })
+    }
+
+    function handleAddClick(event) {
+        console.log('clicked'); 
+    }
     return (
         <>
         <Modal show={show} onHide={handleClose}>
@@ -12,39 +39,46 @@ function NewJobForm({ show, handleClose }) {
             <Modal.Body> 
                 <Row>
                     <Col>
-                    <Form.Group controlId='positionName'>
+                    <Form.Group>
                         <Form.Label>Job Title</Form.Label>
-                        <Form.Control placeholder="ex. 'Web Developer">
-                        </Form.Control>
+                        <Form.Control value={formJob.title} id='title' placeholder="ex. 'Web Developer" onChange={handleFormChange}></Form.Control>
                     </Form.Group>
-                    <Form.Group controlId='companyName'>
+                    <Form.Group >
                         <Form.Label>Company Name</Form.Label>
-                        <Form.Control placeholder="ex. 'Microsoft'" value=''>
-                        </Form.Control>
+                        <Form.Control id='company' value={formJob.company} placeholder="ex. 'Microsoft'" onChange={handleFormChange}></Form.Control>
                     </Form.Group>
                     </Col>
                     <Col>
-                    <Form.Group controlId='companyLoc'>
+                    <Form.Group >
                         <Form.Label>Location</Form.Label>
-                        <Form.Control placeholder="ex. 'Cleveland, OH'" value=''>
-                        </Form.Control>
+                        <Form.Control id='location' value={formJob.location} placeholder="ex. 'Cleveland, OH'" onChange={handleFormChange}></Form.Control>
                     </Form.Group>
                     </Col>
                 </Row>
                 <hr/>
                 <Row>
                     <Col>
-                    <Form.Group controlId='contactName'>
+                    <Form.Group>
                         <Form.Label>Contact Name</Form.Label>
-                        <Form.Control value=''></Form.Control>
+                        <Form.Control id='contactName' value={formJob.contactName} onChange={handleFormChange}></Form.Control>
                     </Form.Group>
                     </Col>
                     <Col>
-                    <Form.Group controlId='companySite'>
+                    <Form.Group >
                         <Form.Label>Company Site</Form.Label>
-                        <Form.Control type='url'></Form.Control>
+                        <Form.Control type='url' id='companySite' value={formJob.companySite} onChange={handleFormChange}></Form.Control>
                     </Form.Group>
                     </Col>
+                </Row>
+                <hr/>
+                <Row>
+                    <Form.Group >
+                        <Form.Label>Technologies</Form.Label>
+                        <InputGroup className='mb-3'>
+                            <Form.Control id='technologies' value={technology} onChange={(e)=>setTechnology(e.target.value)}></Form.Control>
+                            <Button variant='outline-primary' onClick={handleAddClick}>Add</Button>
+                        </InputGroup>
+                    </Form.Group>
                 </Row>
             </Modal.Body>
 
@@ -55,6 +89,7 @@ function NewJobForm({ show, handleClose }) {
             <Button variant="primary" onClick={handleClose}>
                 Save Changes
             </Button>
+            <Button variant='outline-primary' onClick={()=>console.log(formJob, technology)}>Show state variable</Button>
             </Modal.Footer>
         </Modal>
         </>
